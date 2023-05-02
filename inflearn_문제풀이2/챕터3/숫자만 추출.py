8af42781f21e767c5f27d6259f36edef2a4e86b8
정수범 <snowdrop6342@gmail.com>
2023-05-02 23:10:37 +0900

    5.2일자 알고리즘 풀이

---
+++ inflearn_문제풀이2/챕터3/숫자만 추출.py
@@ -0,0 +1,17 @@
+import math
+word = input()
+
+str_number = ""
+for x in word:
+    if x.isdigit():
+        str_number += x
+
+result = int(str_number)
+print(result)
+
+count = 0
+for i in range(1, math.floor(math.sqrt((result)))):
+    if result % i == 0:
+        count += 1
+
+print(count * 2)
